ab71939d791df2321c673bdd5b77d7d5aa6f1547
HadjerBr <hadjerbrioua@gmail.com>
2023-08-10 15:49:45 +0300

    password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+
+
+
 const User = require("../models/user");
 
 function hundelErrors(err) {
--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
+// hashing password mongoose hooks => to fire a function after saving for example
+// we use pre hook and bcrypt to hash the passwords before saving them the the database
+// salt + hashing algorithm
+
+
 const mongoose = require("mongoose");
 const validator = require("validator");
-
+const bcrypt = require("bcrypt");
 const schema = new mongoose.Schema({
     email: {
         type: String,
@@ -24,6 +29,13 @@ const schema = new mongoose.Schema({
     
 
 })
+
+schema.pre("save", async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
 
 const User = new mongoose.model("user", schema);
 
